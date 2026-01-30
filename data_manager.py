e719f5122af13f0b953b61dfa625c92406a7151a
Fabienne Kuntz <fabiennekuntz@Mac-mini-von-Fabienne.fritz.box>
2026-01-30 12:28:49 +0100

    Define SQLAlchemy ORM models (User, Movie), implement basic CRUD methods in DataManager and setup OMDb API connection

---
+++ data_manager.py
@@ -0,0 +1,64 @@
+from models import db, User, Movie
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MOVIES_API_KEY = os.getenv("MOVIES_API_KEY")
+
+
+class DataManager():
+  # Define Crud operations as methods
+    def create_user(self, name):
+        new_user = User(name=name)
+        db.session.add(new_user)
+        db.session.commit()
+
+
+    def get_users(self):
+        return User.query.all()
+
+
+    def get_movies(self, user_id):
+        return Movie.query.filter_by(user_id=user_id).all()
+
+
+    def add_movie(self, user_id, movie):
+        # 1. Filminfos von OMDb holen
+        api_url = f"http://www.omdbapi.com/?t={movie}&apikey={MOVIES_API_KEY}"
+        response = requests.get(api_url)
+        if response.status_code != 200:
+            print("Error retrieving movie information")
+            return None
+
+        data = response.json()
+        if data.get("Response") != "True":
+            print("Movie not found.")
+            return None
+
+        movie = Movie(
+            title=data.get("Title"),
+            year=int(data.get("Year")) if data.get("Year") else None,
+            genre=data.get("Genre"),
+            user_id=user_id
+        )
+
+        db.session.add(movie)
+        db.session.commit()
+
+        return movie
+
+
+    def update_movie(self, movie_id, new_title):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            movie.title = new_title
+            db.session.commit()
+
+
+    def delete_movie(self, movie_id):
+        movie = Movie.query.get(movie_id)
+        if movie:
+            db.session.delete(movie)
+            db.session.commit()
+
--- models.py
+++ models.py
@@ -1,3 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+
+
+class Movie(db.Model):
+    # Define all the Movie properties
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    year = db.Column(db.Integer)
+    # Link Movie to User
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
