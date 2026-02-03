529d54e114e4bf0a235a79061ab4d8880fdc3905
Fabienne Kuntz <fabiennekuntz@Mac-mini-von-Fabienne.fritz.box>
2026-02-03 12:04:27 +0100

    Add /users route to list all users using DataManager

--- models.py
+++ models.py
@@ -7,6 +7,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
 
+    def __repr__(self):
+        return f"<User id={self.id} name='{self.name}'>"
+
 
 class Movie(db.Model):
     # Define all the Movie properties
