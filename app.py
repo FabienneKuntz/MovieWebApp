420605b1af53c7238489cde87617a11ecc150cd2
Fabienne Kuntz <fabiennekuntz@Mac-mini-von-Fabienne.fritz.box>
2026-02-03 14:11:35 +0100

    Implement index page with user list and create_user form; add get_movies route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from data_manager import DataManager
 from models import db, Movie, User
 import os
@@ -18,31 +18,40 @@ db.init_app(app)  # Link the database and the app. This is the reason you need t
 data_manager = DataManager() # Create an object of your DataManager class
 
 
-@app.route('/', METHODS=['GET'])
-def home():
-    return "Welcome to MoviWeb App!"
+@app.route('/', methods=['GET'])
+def index():
+    users = data_manager.get_users()
+    return render_template('index.html', users=users)
 
 
 @app.route('/users', methods=['POST'])
-def list_users():
-    users = data_manager.get_users()
-    return str(users)
+def create_user():
+    name = request.form['name']
+    data_manager.create_user(name)
+    return redirect(url_for('index'))
 
 
 @app.route('/users/<int:user_id>/movies', methods=['GET'])
-pass
+def get_movies(user_id):
+    movies = data_manager.get_movies(user_id)
+    user = next((u for u in data_manager.get_users() if u.id == user_id), None)
+
+    if not user:
+        return "User not found", 404
+
+    return render_template('movies.html', user=user, movies=movies)
 
 
-@app.route('/users/<int:user_id>/movies', methods=['POST'])
-pass
-
-
-@app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
-pass
-
-
-@app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
-pass
+# @app.route('/users/<int:user_id>/movies', methods=['POST'])
+# pass
+#
+#
+# @app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
+# pass
+#
+#
+# @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
+# pass
 
 
 if __name__ == '__main__':
